8b31f721ecffc979662e8069bfa6a46e00654856
Alexander Strakh <strakh@ispras.ru>
2018-04-24 17:54:45 +0300

    Fixes

--- contracts/ITO.sol
+++ contracts/ITO.sol
@@ -1,13 +1,25 @@
 pragma solidity ^0.4.18;
 
 import './AssembledCommonSale.sol';
+import './SoftcapFeature.sol';
 import './ExtendedWalletsMintTokensFeature.sol';
 
-contract ITO is ExtendedWalletsMintTokensFeature, AssembledCommonSale {
+contract ITO is ExtendedWalletsMintTokensFeature, SoftcapFeature, AssembledCommonSale {
+
+  function mintTokensByETH(address to, uint _invested) internal returns(uint) {
+    uint _tokens = super.mintTokensByETH(to, _invested);
+    updateBalance(to, _invested);
+    return _tokens;
+  }
 
   function finish() public onlyOwner {
-     mintExtendedTokens();
-     token.finishMinting();
+    if (updateRefundState()) {
+      token.finishMinting();
+    } else {
+      withdraw();
+      mintExtendedTokens();
+      token.finishMinting();
+    }
   }
 
 }
